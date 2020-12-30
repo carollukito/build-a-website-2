0e2ed2b9a3fc7ed845338284890ba8e47d5412da
carolls07 <76416121+carolls07@users.noreply.github.com>
2020-12-30 12:11:57 +0700

    make new component City.js, modify the Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect, useMemo } from "react";
-// import axios from "axios";
-// import { useHistory } from "react-router-dom";
+//import axios from "axios";
+//import {useHistory} from "react-router-dom";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
+
+import City from "../components/City"
 
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
-}
+  return(
+    // Container
+    <div className="flex flex-col h-screen">
 
-export default Home;
+      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-purple-500"}/>
+
+      <City cityName={"Jakarta"} temp={"24°C"} color={"bg-red-500"} />
+
+      <City cityName={"Bandung"} temp={"17°C"} color={"bg-blue-500"} />
+
+    </div>
+  );
+
+  }
+
+
+  export default Home;
+
+
+
+ 
