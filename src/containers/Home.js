f3b11ecb78243013f82331fd571e579d84e559ed
carolls07 <76416121+carolls07@users.noreply.github.com>
2020-12-30 16:21:01 +0700

    changes a background color

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,30 +5,22 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
-import City from "../components/City"
+import City from "../components/City";
 
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
-  return(
+  return (
     // Container
     <div className="flex flex-col h-screen">
+      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-purple-500"} />
 
-      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-purple-500"}/>
-
-      <City cityName={"Jakarta"} temp={"24°C"} color={"bg-red-500"} />
+      <City cityName={"Jakarta"} temp={"24°C"} color={"bg-pink-500"} />
 
       <City cityName={"Bandung"} temp={"17°C"} color={"bg-blue-500"} />
-
     </div>
   );
+}
 
-  }
-
-
-  export default Home;
-
-
-
- 
+export default Home;
